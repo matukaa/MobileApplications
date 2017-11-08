326704b6f6976c6a923819716389244430aaa38d
matukaa <matukaa9@gmail.com>
2017-11-08 21:56:33 +0200

    Android: Add possibility to edit songs on click

--- MShare/app/src/main/java/com/freakz/matukaa/mshare/SongList.java
+++ MShare/app/src/main/java/com/freakz/matukaa/mshare/SongList.java
@@ -1,10 +1,15 @@
 package com.freakz.matukaa.mshare;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+
+import java.util.List;
 
 public class SongList extends AppCompatActivity {
 
@@ -19,7 +24,33 @@ public class SongList extends AppCompatActivity {
         Song[] songs = { new Song("Title1", "Artist1", "Album1", "Link1"),
                         new Song("Title2", "Artist2", "Album2", "Link2")};
 
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            if (extras.containsKey("id")){
+                int id = extras.getInt("id");
+                songs[id].setTitle(extras.getString("title"));
+                songs[id].setAlbum(extras.getString("album"));
+                songs[id].setArtist(extras.getString("artist"));
+                songs[id].setYt_link(extras.getString("ytlink"));
+            }
+        }
         ListAdapter adapter = new SongAdapter(this, songs);
         song_list_view.setAdapter(adapter);
+
+        song_list_view.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        Song song = (Song)adapterView.getItemAtPosition(i);
+                        Intent editSong = new Intent(view.getContext(), EditSong.class);
+                        editSong.putExtra("title", song.getTitle());
+                        editSong.putExtra("artist", song.getArtist());
+                        editSong.putExtra("album", song.getAlbum());
+                        editSong.putExtra("ytlink", song.getYt_link());
+                        editSong.putExtra("id", i);
+                        startActivity(editSong);
+                    }
+                }
+        );
     }
 }
